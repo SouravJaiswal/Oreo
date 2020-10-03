8fe3a99ed7c5b064abf0add1f2f3c4ff08fa4f4e
Sourav Jaiswal <jsourav01@gmail.com>
2020-10-03 21:44:04 +0530

    Notification build

---
+++ build/Notification/Notification.d.ts
@@ -0,0 +1,3 @@
+import { NotificationProps } from "./Notification.types";
+import "./notification.scss";
+export declare const Notification: ({ toastList, position, autoDelete, dismissTime, }: NotificationProps) => JSX.Element;
