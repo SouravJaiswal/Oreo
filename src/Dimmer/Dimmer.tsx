09fbc2521212757f97ac91a99f3eb4f89d1e0d5d
Sourav Jaiswal <jsourav01@gmail.com>
2020-10-14 09:45:56 +0530

    Dimmer isLoading

--- src/Dimmer/Dimmer.tsx
+++ src/Dimmer/Dimmer.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
 interface Dimmer {
   children: React.ReactNode;
   loading: React.ReactNode;
+  isLoading: boolean;
 }
 
-export const Dimmer = (props: any) => {
+export const Dimmer = (props: Dimmer) => {
   return (
     <div className="dimmer">
       <div className="dimmer__bg"></div>
       {props.children}
-      <div className="dimmer__loading">{props.loading}</div>
+      {props.isLoading ? (
+        <div className="dimmer__loading">{props.loading}</div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
