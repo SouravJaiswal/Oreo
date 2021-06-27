466ce9b3aef6c025ac66cca828dd89eb55560599
Vikram Singh Choudhary <vikramsciitk@gmail.com>
2021-06-28 00:25:41 +0530

    Fix the Table rows issue

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -3,49 +3,51 @@ import React from "react";
 import "./table.scss";
 
 export const Table = ({
-  headings,
-  values,
-  className,
-}: {
-  headings: Array<string>;
-  values: Array<
-    Array<{
+                        headings,
+                        values,
+                        className,
+                      }: {
+    headings: Array<string>;
+    values: Array<Array<{
       value: any;
       render?: Function;
       isHeading?: boolean;
-    }>
-  >;
-  className?: string;
-}) => {
-  let headingJsx = [];
-  let bodyJsx = [];
+    }>>;
+    className?: string;
+  }) => {
+    let headingJsx = [];
+    let bodyJsx = [];
 
-  _.map(headings, (heading, index) => {
-    headingJsx.push(<th>{heading}</th>);
-
-    bodyJsx.push(
-      <tr>
-        {_.map(values[index], (data) => {
-          return !!data.render ? (
-            <td className={data.isHeading ? "table__heading" : ""}>
-              {data.render(data.value)}
-            </td>
-          ) : (
-            <td className={data.isHeading ? "table__heading" : ""}>
-              {data.value}
-            </td>
-          );
-        })}
-      </tr>
+    headings.forEach(heading =>
+      headingJsx.push(<th>{heading}</th>)
     );
-  });
 
-  return (
-    <table className={`table ${className}`}>
-      <thead className="table__head">
+    values.forEach(row => {
+      bodyJsx.push(
+        <tr>
+          {_.map(row, data => {
+            return !!data.render ? (
+              <td className={data.isHeading ? "table__heading" : ""}>
+                {data.render(data.value)}
+              </td>
+            ) : (
+              <td className={data.isHeading ? "table__heading" : ""}>
+                {data.value}
+              </td>
+            );
+          })}
+        </tr>
+      );
+
+    });
+
+    return (
+      <table className={`table ${className}`}>
+        <thead className="table__head">
         <tr>{headingJsx}</tr>
-      </thead>
-      <tbody className="table__body">{bodyJsx}</tbody>
-    </table>
-  );
-};
+        </thead>
+        <tbody className="table__body">{bodyJsx}</tbody>
+      </table>
+    );
+  }
+;
